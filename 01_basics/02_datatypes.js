57bad18ed522e13969257c746c6414b1cdfadb09
artist0909 <134792525+artist0909@users.noreply.github.com>
2025-05-31 07:22:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+let name = "Devashish"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+//Datatypes
+
+//number :- 2 to power 53
+//bigInt
+//string :- "",''
+//boolean :- true/false
+//null :- standalone value
+//undefined :- value not asign
+//symbol :- uniqe
+
+//object
+
+console.log(typeof undefined)// undefined is type thats way if you run this it sows undefinded
+console.log(typeof null)// null is object thats way if you run this its shows object not shows null
