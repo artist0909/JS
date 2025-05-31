ee36f3778c85d175b8df24548ef80ada5f64eb3f
artist0909 <134792525+artist0909@users.noreply.github.com>
2025-05-31 07:47:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,28 @@
+const { StrictMode } = require("react")
+
+let score = "dev"
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"7" -> 7
+//"7abc" -> NaN
+//true -> 1 false -> 0
+let isLoggedIn = null
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 -> true 0 -> false
+//"" -> false 
+//"dev" -> true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
